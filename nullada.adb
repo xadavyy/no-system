2f076fdfeb880e35e80dc8ba64d8bcb67be2d7ee
gnadavydz <david.zucca69@gmail.com>
2015-03-13 01:15:37 +0100

    Create nullada.adb
    testing eQommunicabilities, hum...

---
+++ nullada.adb
@@ -0,0 +1,12 @@
+--| zero step programing with GNAT ADA200
+pragma ada_2005;
+pragma licence(unrestricted);
+--|
+procedure nullada is
+  begin
+    null;
+  end nullada;
+
+--| i have to found another way to schribt anyway but by webeditors
+--| ... from a Samsung TAB310 is very baad as evil ?o(
+
