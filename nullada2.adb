6a7276909ba8a225db2b97d66357291bcb318136
gnadavydz <david.zucca69@gmail.com>
2015-03-29 16:58:31 +0200

    Create nullada2.adb

---
+++ nullada2.adb
@@ -0,0 +1,13 @@
+--| zero step programing with GNAT ADA200
+pragma ada_2005;
+pragma licence(unrestricted);
+--|
+procedure nullada2 is
+  begin
+    null;
+  end nullada2;
+
+--| i have to found another way to schribt anyway but by webeditors
+--| ... from a Samsung TAB310 is very baad as evil ?o(
+
+-- toto
